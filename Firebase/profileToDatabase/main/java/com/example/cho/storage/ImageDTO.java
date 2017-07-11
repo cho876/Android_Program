9ab693fb286d20d9160082415acdbe74ed007fe3
Cho-PC\Cho <weapon114@naver.com>
2017-07-11 23:46:44 +0900

    <안드로이드 연습> FirebaseStorage을 활용한 이미지 사진 업로드 및 FirebaseDatabase 내 프로필 저장

---
+++ Firebase/profileToDatabase/main/java/com/example/cho/storage/ImageDTO.java
@@ -0,0 +1,13 @@
+package com.example.cho.storage;
+
+/**
+ * Created by Cho on 2017-07-11.
+ */
+
+public class ImageDTO {
+    public String ImageUri;
+    public String title;
+    public String description;
+    public String uid;
+    public String userId;
+}
