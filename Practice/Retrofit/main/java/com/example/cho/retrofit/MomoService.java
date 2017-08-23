368746538e6346cb6a76884248bb4e65f736e8f2
Cho-PC\Cho <weapon114@naver.com>
2017-08-23 23:43:24 +0900

    <안드로이드 연습> Retrofit2를 활용한 HTTP API로부터 데이터 GET

--- Practice/Retrofit/main/java/com/example/cho/retrofit/MomoService.java
+++ Practice/Retrofit/main/java/com/example/cho/retrofit/MomoService.java
@@ -5,25 +5,21 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
 
 /**
- * Created by Cho on 2017-08-22.
+ * Retrofit 기본 Setting Interface
+ *
+ * @author SungkwonCho
  */
-
 public interface MomoService {
 
-    /*  Select  (http://api.github.com/repos/{owner}/{repo}/contributors*/
-    @GET("repos/{owner}/{repo}/contributors")
-    Call<List<Users>> getUsers(             // => getUsers(String owner, String repo)
-            @Path("owner") String owner
-            , @Path("repo") String repo);
+    @GET("/repos/square/retrofit/contributors")
+    Call<List<Stores>> getStoreList();
 
-    public static final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://api.github.com/")
-            .addConverterFactory(GsonConverterFactory.create()) // Gson (json To Java Object)
+    public static final Retrofit gitRetrofit = new Retrofit.Builder()
+            .baseUrl("https://api.github.com")
+            .addConverterFactory(GsonConverterFactory.create())
             .build();
+
 }
