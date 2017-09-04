21f144a53d0a123be5fe8add59e4c6978988b3e1
Cho-PC\Cho <weapon114@naver.com>
2017-09-04 14:15:32 +0900

    <안드로이드 프로젝트> "IoT 스마트 멀티탭"  (DB 내 콘센트 상태 저장 Thread)

--- Project/IoT_Multitap/android/java/JoinDbThread.java
+++ Project/IoT_Multitap/android/java/JoinDbThread.java
@@ -20,47 +20,45 @@ public class JoinDbThread extends Thread {
     private Context context;
     private RegisterRequest registerRequest;
     private String className;
+    private int index;
 
     public JoinDbThread(Context context, RegisterRequest registerRequest) {
         this.context = context;
-        className = context.getClass().getSimpleName().toLowerCase().substring(0,5);
+        className = context.getClass().getSimpleName().toLowerCase().substring(0, 5);
+        Log.e("JoinDBThread", context.getClass().toString());
         sharedPreferenceGetSet = new SharedPreferenceGetSet(context);
         this.registerRequest = registerRequest;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
     }
 
     @Override
     public void run() {
         try {
-            while (true) {
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if (success)
-                                Log.e("JoinDbThread", "성공적으로 DB 저장");
-                            else
-                                Log.e("JoinDbThread", "DB 저장 실패");
-                        } catch (Exception e) {
-                            Log.e("JoinDbThread", "ERROR");
-                        }
+            Response.Listener<String> responseListener = new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        boolean success = jsonResponse.getBoolean("success");
+                        if (success)
+                            Log.e("JoinDbThread", "성공적으로 DB 저장");
+                        else
+                            Log.e("JoinDbThread", "DB 저장 실패");
+                    } catch (Exception e) {
+                        Log.e("JoinDbThread", "ERROR");
                     }
-                };
-                registerRequest = new RegisterRequest(sharedPreferenceGetSet.getPrefBoolean("consent_1_"+className),
-                        sharedPreferenceGetSet.getPrefBoolean("consent_2_"+className),
-                        sharedPreferenceGetSet.getPrefBoolean("consent_3_"+className),
-                        sharedPreferenceGetSet.getPrefBoolean("consent_4_"+className),
-                        responseListener);
+                }
+            };
+            registerRequest = new RegisterRequest("consent" + index,
+                    sharedPreferenceGetSet.getPrefBoolean("consent_" + index + "_" + className),
+                    responseListener);
 
-                RequestQueue queue = Volley.newRequestQueue(context);
-                queue.add(registerRequest);
-                Log.e("DATA",sharedPreferenceGetSet.getPrefBoolean("consent_1_"+className)+", "+
-                        sharedPreferenceGetSet.getPrefBoolean("consent_2_"+className)+", "+
-                        sharedPreferenceGetSet.getPrefBoolean("consent_3_"+className)+", "+
-                        sharedPreferenceGetSet.getPrefBoolean("consent_4_"+className));
-                Thread.sleep(2000);
-            }
+            Log.e("THREAD", "consent" + index+", "+sharedPreferenceGetSet.getPrefBoolean("consent_" + index + "_" + className));
+            RequestQueue queue = Volley.newRequestQueue(context);
+            queue.add(registerRequest);
         } catch (Exception e) {
             Log.e("JoinDbThread", "Thread ERROR");
         }
