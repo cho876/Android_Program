c9f062af0ecc5908a2b8847ed01e74f62931896e
Cho-PC\Cho <weapon114@naver.com>
2017-09-04 14:17:01 +0900

    <안드로이드 프로젝트> "IoT 스마트 멀티탭"  (콘센트 타이머 설정에 따른 Notification BroadcastReceiver)

--- Project/IoT_Multitap/android/java/BroadcastR.java
+++ Project/IoT_Multitap/android/java/BroadcastR.java
@@ -6,15 +6,22 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * 알람 시간이 되었을 시, Notification을 통한 동작 Class
  */
 
 public class BroadcastR extends BroadcastReceiver {
+    private JoinDbThread joinDbThread;
+    private RegisterRequest registerRequest;
+
     @Override
-    public void onReceive(Context context, Intent intent){
-        NotificationManager manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+    public void onReceive(Context context, Intent intent) {
+        int index = intent.getExtras().getInt("index");
+        joinDB(context, index);
+
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Intent intent1 = new Intent(context, PowerActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent1, 0);
         Notification.Builder builder = new Notification.Builder(context);
@@ -22,9 +29,16 @@ public class BroadcastR extends BroadcastReceiver {
                 .setWhen(System.currentTimeMillis())
                 .setContentTitle("MULTI TAP")
                 .setContentText("예약 설정 실행 중")
-                .setDefaults(Notification.DEFAULT_SOUND|Notification.DEFAULT_VIBRATE)
+                .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE)
                 .setContentIntent(pendingIntent);
 
         manager.notify(1, builder.build());
+    }
+
+    private void joinDB(Context context, int index) {
+        joinDbThread = new JoinDbThread(context, registerRequest);
+        joinDbThread.setIndex(index);
+        joinDbThread.setDaemon(true);
+        joinDbThread.run();
     }
 }
