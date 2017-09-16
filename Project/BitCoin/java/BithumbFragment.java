bfd61e5fbae31e75b2cd686aab9f06dc786174ca
Cho-PC\Cho <weapon114@naver.com>
2017-09-16 14:55:56 +0900

    <안드로이드 프로젝트> "BitCoin 조회" (Bithumb 정보 View Fragment)

---
+++ Project/BitCoin/java/BithumbFragment.java
@@ -0,0 +1,102 @@
+package com.example.cho.bitcoin;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * 빗썸 정보 Fragment
+ */
+public class BithumbFragment extends Fragment {
+
+    private TextView tv_exchange, tv_kr_max, tv_us_max, tv_kr_min, tv_us_min,
+            tv_kr_change, tv_us_change, tv_kr_premium_change, tv_us_premium_change,
+            tv_percent, tv_premium_percent, tv_volume;
+    private ConvertToJsonThread toJsonThread;
+    private RegisterRequest registerRequest;
+    private JSONObject jsonObject;
+    private Handler handler;
+    private SharedPreferences pref;
+    private String exchangeName;
+    private Data data;
+    private FormatPackage formatPackage;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        pref = this.getContext().getSharedPreferences("Bitcoin", Context.MODE_PRIVATE);
+        exchangeName = pref.getString("exchange", null);
+        View rootView = inflater.inflate(R.layout.fragment_bit_humb, container, false);
+        initView(rootView);
+        initHandler();
+        joinThread(handler);
+        return rootView;
+    }
+
+    /**
+     * UI 초기화
+     *
+     * @param view
+     */
+    private void initView(View view) {
+        tv_exchange = (TextView) view.findViewById(R.id.bithumb_exchange);
+
+        tv_kr_max = (TextView) view.findViewById(R.id.bithumb_kr_max);
+        tv_us_max = (TextView) view.findViewById(R.id.bithumb_us_max);
+
+        tv_kr_min = (TextView) view.findViewById(R.id.bithumb_kr_min);
+        tv_us_min = (TextView) view.findViewById(R.id.bithumb_us_min);
+
+        tv_kr_change = (TextView) view.findViewById(R.id.bithumb_kr_change);
+        tv_us_change = (TextView) view.findViewById(R.id.bithumb_us_change);
+        tv_percent = (TextView) view.findViewById(R.id.bithumb_percent);
+
+        tv_kr_premium_change = (TextView) view.findViewById(R.id.bithumb_kr_premium_change);
+        tv_us_premium_change = (TextView) view.findViewById(R.id.bithumb_us_premium_change);
+        tv_premium_percent = (TextView) view.findViewById(R.id.bithumb_premium_percent);
+
+        tv_volume = (TextView) view.findViewById(R.id.bithumb_volume);
+    }
+
+    private void initHandler() {
+        handler = new Handler() {
+            public void handleMessage(Message msg) {
+                if (msg.what == 0) {
+                    jsonObject = (JSONObject) msg.obj;
+                    data = new Data(jsonObject);
+                    formatPackage = new FormatPackage();
+                    formatPackage.setText(tv_exchange, data.getExchange());
+                    formatPackage.setTextKorea(tv_kr_max, data.getKoreaMax());
+                    formatPackage.setTextUsa(tv_us_max, data.getUsaMax());
+                    formatPackage.setTextKorea(tv_kr_min, data.getKoreaMin());
+                    formatPackage.setTextUsa(tv_us_min, data.getUsaMin());
+                    formatPackage.setTextKorea(tv_kr_premium_change, data.getKoreaPremium());
+                    formatPackage.setTextUsa(tv_us_premium_change, data.getUsaPremium());
+                    formatPackage.setTextPercent(tv_percent, data.getPercent());
+                    formatPackage.setTextPercent(tv_premium_percent, data.getPremiumPercent());
+                    formatPackage.setTextKorea(tv_kr_change, data.getKoreaChangeRate());
+                    formatPackage.setTextUsa(tv_us_change, data.getUsaChangeRate());
+                    formatPackage.setTextVolume(tv_volume, data.getVolume());
+                }
+            }
+        };
+    }
+
+    private void joinThread(Handler handler) {
+        toJsonThread = new ConvertToJsonThread(getContext(), registerRequest, handler);
+        toJsonThread.setCompany(exchangeName);
+        toJsonThread.setTitle("bithumb");
+        toJsonThread.setDaemon(true);
+        toJsonThread.run();
+    }
+}
