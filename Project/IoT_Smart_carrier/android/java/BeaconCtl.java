379b78a14e2c6c66ed149cc636b9f6a96c774860
skCho-PC\skCho <weapon114@naver.com>
2018-01-07 01:26:39 +0900

    <IoT 스마트 캐리어> - Android와 Beacon 간의 통신 제어

--- Project/IoT_Smart_carrier/android/java/BeaconCtl.java
+++ Project/IoT_Smart_carrier/android/java/BeaconCtl.java
@@ -2,8 +2,10 @@ package com.example.skcho.smartcarrier;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -11,6 +13,14 @@ import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.UUID;
 
@@ -65,9 +75,11 @@ public class BeaconCtl {
             @Override
             public void onBeaconsDiscovered(Region region, List<Beacon> list) {
                 if (!list.isEmpty()) {
+                    InsertData task = new InsertData();
                     Beacon nearestBeacon = list.get(0);
                     Log.e("Beacon", "location: " + nearestBeacon.getRssi());
                     tvId.setText(nearestBeacon.getRssi() + "");
+                    task.execute(nearestBeacon.getRssi() + "");
                     if (!isConnected && nearestBeacon.getRssi() > -70) {
                         isConnected = true;
                         AlertDialog.Builder dialog = new AlertDialog.Builder(activity);
@@ -88,5 +100,48 @@ public class BeaconCtl {
         region = new
                 Region("ranged region",
                 UUID.fromString(uuid), 40001, 14903); // 본인이 연결할 Beacon의 ID와 Major / Minor Code를 알아야 한다.
+    }
+
+    class InsertData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String data;
+            String param = "u_rssi=" + params[0] + "";    // 사용자가 기입한 값을 저장한 변수
+            try {
+                URL url = new URL(
+                        "http://192.168.0.6/send_rssi.php");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.connect();
+
+                /* 안드로이드 -> 서버 파라메터 값 전달*/
+                OutputStream outs = conn.getOutputStream();
+                outs.write(param.getBytes("UTF-8"));
+                outs.flush();
+                outs.close();
+
+                InputStream is = null;
+                BufferedReader in = null;
+                data = "";
+
+                is = conn.getInputStream();
+                in = new BufferedReader(new InputStreamReader(is), 8 * 1024);
+                String line = null;
+                StringBuffer buff = new StringBuffer();
+                while ((line = in.readLine()) != null) {
+                    buff.append(line + "\n");
+                }
+                data = buff.toString().trim();
+                Log.e("RESULT", "MSG - " + data);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
