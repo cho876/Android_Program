765ab1f5c0097538b0f94f4594718b1c7f2324a1
skCho-PC\skCho <weapon114@naver.com>
2018-02-11 18:00:07 +0900

    <IoT 스마트 캐리어> - JSON Path Settings

--- Project/IoT_Smart_carrier/android/java/RetrofitService.java
+++ Project/IoT_Smart_carrier/android/java/RetrofitService.java
@@ -1,10 +1,15 @@
 package com.example.skcho.smartcarrier;
 
 
+import com.estimote.sdk.cloud.internal.User;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * Created by Cho on 2017-12-28.
@@ -14,8 +19,18 @@ public interface RetrofitService {
     @GET("/getCartInfoByJson.php")
     Call<CartDAO> getData();
 
+    @GET("/Json.php")
+    Call<List<UserDAO>> getUserId(
+            @Query("userName") String userName,
+            @Query("userEmail") String userEmail);
+
+    @GET("/Json.php")
+    Call<List<UserDAO>> getUserPw(
+            @Query("userEmail") String userEmail,
+            @Query("userID") String userID);
+
     public static final Retrofit gitRetrofit = new Retrofit.Builder()
-            .baseUrl("http://192.168.0.6")
+            .baseUrl("http://192.168.0.5")
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 }
