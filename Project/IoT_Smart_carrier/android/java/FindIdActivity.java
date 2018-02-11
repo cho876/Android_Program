12e047e207b0db0e9dcab07041988a2f5181bd01
skCho-PC\skCho <weapon114@naver.com>
2018-02-11 17:55:49 +0900

    <IoT 스마트 캐리어> - Find ID Activity

---
+++ Project/IoT_Smart_carrier/android/java/FindIdActivity.java
@@ -0,0 +1,119 @@
+package com.example.skcho.smartcarrier;
+
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.tsengvn.typekit.TypekitContextWrapper;
+
+
+/**
+ * 아이디 찾기 Activity
+ * <p>
+ * Retrofit 통신 활용
+ * userName, userEmail을 통한 userID 반환 Activity
+ */
+
+public class FindIdActivity extends AppCompatActivity implements View.OnClickListener {  // "아이디 찾기" 화면
+
+    private Button bLeft, bRight, bSearch;
+    private EditText ed_name, ed_email;    // 이름, 이메일 기입란
+    private TextView text_id;             // 아이디 찾기 결과 변수
+    private CustomButton customButton;
+    private ValidCheck validCheck;
+    private RetrofitConnect retrofitConnect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_id);
+        initView();
+        retrofitConnect = new RetrofitConnect();
+        validCheck = new ValidCheck(this);
+    }
+
+    /**
+     * UI 초기화 함수
+     */
+    public void initView() {
+        ed_name = (EditText) findViewById(R.id.findId_Name);                           // 이름 기입 란
+        ed_email = (EditText) findViewById(R.id.findId_Email);                          // 이메일 기입 란
+
+        text_id = (TextView) findViewById(R.id.searchId_btn);                 // 검색 결과 란
+        bSearch = (Button) findViewById(R.id._findid_search);                   // 아이디 찾기 버튼
+        bSearch.setOnClickListener(this);
+
+        customButton = (CustomButton) findViewById(R.id.custom_findId_btn);      // 뒤로가기, 비밀번호 찾기 버튼
+        bLeft = customButton.getLeftBtn();
+        bLeft.setOnClickListener(this);
+        bRight = customButton.getRightBtn();
+        bRight.setOnClickListener(this);
+    }
+
+    /**
+     * 버튼에 따른 기능 부여
+     * "R.id.searchId_btn" -> "아이디 찾기" Button  (아이디 찾기 결과 출력)
+     * "R.id.button_right" -> "비밀번호 찾기" Button  (비밀번호 찾기 창으로 이동)
+     * "R.id.button_left" -> "돌아가기" Button    (이전 창으로 이동)
+     *
+     * @param view
+     */
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.searchId_btn:
+                editToStr();
+                break;
+            case R.id.button_right:
+                Intent go_pw = new Intent(this, FindPwActivity.class);
+                startActivity(go_pw);
+                break;
+            case R.id.button_left:
+                Intent go_login = new Intent(this, LoginActivity.class);
+                UtilCheck.UtilClose(go_login);
+                startActivity(go_login);
+                break;
+        }
+    }
+
+    /**
+     * EditText로 기입한 데이터 String으로 변환
+     * ConnectToDB로 변환시킨 String값 전달
+     */
+    public void editToStr() {           // DB 내, 저장 Func
+        String sName = ed_name.getText().toString();     // EditText -> String으로 변환
+        String sEmail = ed_email.getText().toString();
+
+        if (validCheck.isWrited(sName, sEmail))        // 두 EditText 모두 기입할 시,
+            connectToDB(sName, sEmail, text_id);
+    }
+
+    /**
+     * Retrofit 통신을 통해 userName, userEmail에 해당하는 userID 반환 및 TextView 수정
+     *
+     * @param userName  (유저 이름)
+     * @param userEmail (유저 이메일)
+     * @param tv        (변경할 TextView)
+     */
+    private void connectToDB(String userName, String userEmail, final TextView tv) {
+        retrofitConnect.getUserIdByAsync(userName, userEmail, new RetrofitConnect.callBack<UserDAO>() {
+            @Override
+            public void execute(UserDAO user) {
+                tv.setText(user.getUserID());
+            }
+        });
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(TypekitContextWrapper.wrap(newBase));
+    }
+}
